5a9c87537971a958a73b88bdf5260151e2fa13d3
Ricky Ng <theprimestrider@gmail.com>
2026-07-15 17:17:31 +0700

    update gitignore

--- src/features/example/views/ExampleView.vue
+++ src/features/example/views/ExampleView.vue
@@ -4,7 +4,6 @@ import ExampleComponent from "../components/ExampleComponent.vue"
 <template>
   <div class="max-w-4xl bg-neutral-800">
     <h1 class="text-3xl text-white">This is Example View</h1>
-
     <ExampleComponent class="mt-5"></ExampleComponent>
   </div>
 </template>
